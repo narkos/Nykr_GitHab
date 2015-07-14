3fb5744bc7bb8d220b5c82f8c1f840813f8a61d6
Fetdressing <simon.home@hotmail.com>
2015-07-14 22:15:44 +0200

    EventsystemINPROGRESS
    kan vara errors atm

---
+++ New Unity Project/Assets/Nykr/NScripts/EventActivater.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventActivater : Event {
+	public GameObject[] objects_Event_Applied; //dessa object är de som kommer stängas on/off
+	public bool activeStart = true; //är dessa object aktiverade från start
+
+	private bool isActive;
+	// Use this for initialization
+	void Start () {
+		Initialize ();
+	}
+
+	public override void Initialize ()
+	{
+		base.Initialize ();
+		Reset ();
+	}
+	
+	public override void StartEvent(){
+		base.StartEvent (); //varför funkar inte cooldown???
+
+		if (isActive == true) {
+			isActive = false;
+			for (int i = 0; i < objects_Event_Applied.Length; i++) {
+				objects_Event_Applied [i].SetActive (false);
+			}
+		} else {
+			isActive = true;
+			for (int i = 0; i < objects_Event_Applied.Length; i++) {
+				objects_Event_Applied [i].SetActive (true);
+			}
+		}
+
+	}
+	public override void PlayerInteract(){
+		StartEvent ();
+	}
+	public override void Interact(){
+		
+	}
+	
+	public override void Reset(){
+		isActive = activeStart;
+
+		for (int i = 0; i < objects_Event_Applied.Length; i++) {
+			objects_Event_Applied[i].SetActive(activeStart);
+		}
+	}
+}
---
+++ New Unity Project/Assets/Nykr/NScripts/PlayerEventChecker.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerEventChecker : MonoBehaviour {
+	private Transform thisTransform; //kommer nog inte vara spelaren utan kameran
+
+	private Collider[] potColliders;
+	public float collisionRadius = 20;
+
+	private Camera mainCamera;
+	private Ray rayMiddleScreen;
+	private RaycastHit hit;
+
+	private Event currentInteractEvent; //det eventet som saker kommer hända på vid denna frame
+	public float interactRange = 10;
+	public KeyCode interactKey = KeyCode.E;
+	// Use this for initialization
+	void Start () {
+		mainCamera = Camera.main;
+		thisTransform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckForEvent ();
+
+		if (currentInteractEvent != null) {
+			currentInteractEvent.Interact(); //kallas varje frame bara man kolliderar eller liknande
+			if (Input.GetKeyDown (interactKey)) {
+				currentInteractEvent.PlayerInteract ();
+			}
+		}
+
+
+	}
+
+
+	void CheckForEvent(){
+		if (CheckRaycast () == true)
+			return;
+		if (CheckCollision () == true)
+			return;
+
+		//om den inte hitta nått event
+		currentInteractEvent = null;
+	}
+
+	bool CheckRaycast(){
+		rayMiddleScreen = mainCamera.ViewportPointToRay (new Vector3 (0.5f, 0.5f, 0.5f));
+		if (Physics.Raycast (rayMiddleScreen, out hit, interactRange)) {
+			if (hit.collider.tag == "Event") {
+				currentInteractEvent = hit.collider.GetComponent<Event> ();
+				if(currentInteractEvent.is_raycastEvent == true){
+					return true;
+				}
+				else{
+					currentInteractEvent = null;
+					return false;
+				}
+			}
+
+		}
+		return false;
+	}
+
+	bool CheckCollision(){
+		potColliders = Physics.OverlapSphere (thisTransform.position, collisionRadius);
+		int i = 0;
+		bool checker = false;
+		while (i < potColliders.Length) {
+			if(potColliders [i].tag == "Event"){
+				currentInteractEvent = potColliders[i].GetComponent<Event>();
+				if(currentInteractEvent.is_collideEvent == true){
+					return true;
+				}
+				else{
+					currentInteractEvent = null;
+					return false;
+				}
+			}
+			i++;
+		}
+		return false;
+	}
+
+}
